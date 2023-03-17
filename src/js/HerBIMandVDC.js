550765f1b82a2c2b6453c947c762d9c655cc3863
Evhenii Babii <babiyka09@gmail.com>
2023-03-17 15:05:33 +0200

    add mediaScreen and fixed arrow-line

--- src/js/HerBIMandVDC.js
+++ src/js/HerBIMandVDC.js
@@ -1,30 +1,30 @@
 import BIMlist from '../templates/HerBIMAndVDCServices.hbs'
-const dataExample = [
+const dataBIM = [
     {
-      subtitle: 'The client contacts our team.',
+      boldText: 'Boost design speed. ',
       description:
-        'Boost design speed. BIM modeling and clash detection eliminate up to 90% of design errors, increasing productivity up to fivefold.',
+        'BIM modeling and clash detection eliminate up to 90% of design errors, increasing productivity up to fivefold.',
+
     },
     {
-      subtitle: 'The client contacts our team.',
+        boldText: 'Create an accurate cost plan. ',
       description:
-        'Create an accurate cost plan. We can help you identify precise budget, cashflow, and time estimates for your projects.',
+        'We can help you identify precise budget, cashflow, and time estimates for your projects.',
     },
     {
-      subtitle: 'The client contacts our team.',
+        boldText: 'Save time and money. ',
       description:
-        'Save time and money. Innovative coordination software and expertise help us detect soft (access) and hard (geometry) clashes to reduce expenses during the modeling, construction, and maintenance stages.',
+        'Innovative coordination software and expertise help us detect soft (access) and hard (geometry) clashes to reduce expenses during the modeling, construction, and maintenance stages.',
     },
     {
-      subtitle: 'The client contacts our team.',
+        boldText: 'Improve collaboration. ',
       description:
-        'Improve collaboration. Architects, engineers, designers, and clients can coordinate changes in real-time using proven workflow designs and cloud solutions.',
+        'Architects, engineers, designers, and clients can coordinate changes in real-time using proven workflow designs and cloud solutions.',
     },
   ];
 
 const cardList = document.querySelector('.BIMServices__list')
-console.log(cardList);
 export function appendCardMarkup(dataArr, element) {
     element.insertAdjacentHTML('beforeend', BIMlist(dataArr));
   }
-  appendCardMarkup(dataExample, cardList);
+  appendCardMarkup(dataBIM, cardList);
