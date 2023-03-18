c77f6faa85ff60b89eb1fad6b0f9fc5b5485f273
Alex Koniushenko <alex.koniushenko@gmail.com>
2023-03-18 13:22:17 +0200

    hbs +html done

---
+++ src/js/FAQ.js
@@ -0,0 +1,11 @@
+import FAQ from '../templates/FAQ.hbs';
+const questExample = [
+  'How long does it take to produce an architectural 3D render?',
+  'How much do you charge for architectural rendering?',
+  'What do you need for architectural rendering?',
+  'What can I adjust for rendering?',
+  'Do you offer free revisions?',
+];
+const faqSection = document.querySelector('.faq');
+console.log(faqSection);
+faqSection.insertAdjacentHTML('beforeend', FAQ(questExample));
