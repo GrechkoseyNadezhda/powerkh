7b828d80e36c5b26b0d884321406a1963243b320
Evgeniy Pavlichenko <yevheniipavl@gmail.com>
2023-03-20 22:41:08 +0200

    creat bim files

---
+++ src/js/bim-content/bim-content-sections/bim-content-data.js
@@ -0,0 +1,28 @@
+export const advantages = {
+  title: 'Powerkh Adds Value to Your Project',
+  description:
+    'We do more than detecting clashes and generating reports — we know how to resolve your problems and improve the construction project. Powerkh will coordinate your project to help you reduce expenses and improve turnaround time.',
+  listDescription: 'What sets us apart from the competition?',
+  items: [
+    {
+      subtitle: 'Engineering expertise',
+      subtitleDescription:
+        'Our professionals have a wide range of engineering qualifications to optimize drawings and 3D construction models.',
+    },
+    {
+      subtitle: 'Technological proficiency',
+      subtitleDescription:
+        'We use advanced BIM modeling and clash detection software to eliminate design errors before construction begins.',
+    },
+    {
+      subtitle: 'Legislative framework',
+      subtitleDescription:
+        'Our experts will ensure that your project’s elements adhere to governmental requirements and standards.',
+    },
+    {
+      subtitle: 'Customer satisfaction',
+      subtitleDescription:
+        'Many companies reduced work hours and production costs by following our proven methodologies and software solutions.',
+    },
+  ],
+};
