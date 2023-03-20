7bd19e1dc6482a07c9eb3bab5b8c77f89fba7980
Alex Koniushenko <alex.koniushenko@gmail.com>
2023-03-20 22:23:57 +0200

    script added svg renamed

--- src/js/archi-rendering/archi-rendering-section/archi-rendering-data.js
+++ src/js/archi-rendering/archi-rendering-section/archi-rendering-data.js
@@ -59,39 +59,36 @@ export const BIMservices = {
   ],
 };
 
-export const howCreates = [
-  {
-    subtitle: 'The client reaches out to us.',
-    description:
-      'You can fill the form on our website, and we will get back to you immediately.',
-  },
-  {
-    subtitle: 'We outline the project plan.',
-    description:
-      'We discuss the deliverables and specifications, such as resolutions, camera angles, textures, and lighting.',
-  },
-  {
-    subtitle: 'We receive reference materials.',
-    description:
-      'You will provide us with a 3D model of your project with optional architectural drawings, photos, and other reference materials.',
-  },
-  {
-    subtitle: 'You receive the first draft.',
-    description:
-      'We start working on a model and provide the first draft in advance to incorporate your feedback into the final design.',
-  },
-  {
-    subtitle: 'The client gets the final project.',
-    description:
-      'Our team delivers final 2D & 3D architectural renders within a specified time (or sooner).',
-  },
-  // {
-  //   subtitle:
-  //     'The client receives interactive 3D BIM models and shop drawings.',
-  //   description:
-  //     'You will get accurate fabrication drawings and a fully interactive 3D database that can tie construction and manufacturing sites for a significant productivity boost.',
-  // },
-];
+export const howCreates = {
+  title: 'How Powerkh Creates Architectural Renderings',
+  list: [
+    {
+      subtitle: 'The client reaches out to us.',
+      description:
+        'You can fill the form on our website, and we will get back to you immediately.',
+    },
+    {
+      subtitle: 'We outline the project plan.',
+      description:
+        'We discuss the deliverables and specifications, such as resolutions, camera angles, textures, and lighting.',
+    },
+    {
+      subtitle: 'We receive reference materials.',
+      description:
+        'You will provide us with a 3D model of your project with optional architectural drawings, photos, and other reference materials.',
+    },
+    {
+      subtitle: 'You receive the first draft.',
+      description:
+        'We start working on a model and provide the first draft in advance to incorporate your feedback into the final design.',
+    },
+    {
+      subtitle: 'The client gets the final project.',
+      description:
+        'Our team delivers final 2D & 3D architectural renders within a specified time (or sooner).',
+    },
+  ],
+};
 
 export const whatWeOffer = {
   title: 'What We Offer:',
