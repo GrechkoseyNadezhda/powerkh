e3fa5c63bd7966746591a35e58c0fb4a04f782b3
Alex Koniushenko <alex.koniushenko@gmail.com>
2023-03-20 19:08:37 +0200

    How Powerh Creates section done

--- src/js/archi-rendering/archi-rendering-section/archi-rendering-templates.js
+++ src/js/archi-rendering/archi-rendering-section/archi-rendering-templates.js
@@ -1,9 +1,17 @@
 import heroTemplate from '../../../templates/hero.hbs';
+import howWeWorkTemplate from '../../../templates/howWeWork.hbs';
+
 import faqTemplate from '../../../templates/faq.hbs';
 import { refs } from '../../common/refs-services';
 import { useTemplate } from '../../common/use-template';
 import { toggleFaq } from '../../common/toggle-faq';
-import { archiRenderingFaq, archiRenderingHero } from './archi-rendering-data';
+import {
+  archiRenderingFaq,
+  archiRenderingHero,
+  howCreates,
+} from './archi-rendering-data';
+
 useTemplate(refs.hero, heroTemplate, archiRenderingHero);
+useTemplate(refs.howWeWork, howWeWorkTemplate, howCreates);
 useTemplate(refs.faq, faqTemplate, archiRenderingFaq);
 toggleFaq();
