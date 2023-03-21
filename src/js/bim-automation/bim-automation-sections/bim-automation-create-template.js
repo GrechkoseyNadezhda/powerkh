9440a82727eb523934866fee9f4545a63e515ab0
Oleksandr Bron <lexlutermotion@gmail.com>
2023-03-21 19:10:20 +0200

    prepared bim-automation template for import

--- src/js/bim-automation/bim-automation-sections/bim-automation-create-template.js
+++ src/js/bim-automation/bim-automation-sections/bim-automation-create-template.js
@@ -1,4 +1,10 @@
+import howWeWorkHBS from '../../../templates/howWeWork.hbs';
 import servicesHBS from '../../../templates/architecAndStructBim.hbs';
+import heroHBS from '../../../templates/hero.hbs';
+import featuresForProjectsHBS from '../../../templates/outsourceProjects.hbs';
+import contactUsHBS from '../../../templates/contactUs.hbs';
+import briefDescriptHBS from '../../../templates/herBIMAndVDCServices.hbs';
+import faqHBS from '../../../templates/faq.hbs';
 import { refs } from '../../common/refs-services';
 import { useTemplate } from '../../common/use-template';
 import { architecAndStruct } from './bim-automation-data';
