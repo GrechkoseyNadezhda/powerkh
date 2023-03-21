f12b2170c31af6c681bad4611fb5bfc9f9f2f18a
Evhenii Babii <babiyka09@gmail.com>
2023-03-21 15:46:02 +0200

    fix mediarules and template

--- src/js/bim-modeling/bim-modeling-section/bim-modeling-data.js
+++ src/js/bim-modeling/bim-modeling-section/bim-modeling-data.js
@@ -34,38 +34,37 @@
 
   }
 
-  export const howModeling = [
-    {
-      subtitle: 'The client sends the brief and technical documentation.',
-      description:
-        'We discuss minute details of the construction project and analyze drawings, 2D plans, blueprints, photos, and visuals references.',
-    },
-    {
-      subtitle: 'We import 2D drawings into the 3D model.',
-      description:
-        'Our team transforms flat drawings into a 3D model using Autodesk Revit software.',
-    },
-    {
-      subtitle: 'We build the framework and design elements.',
-      description:
-        'We digitalize all structural elements — from walls and MEP (mechanical, electrical, and plumbing) systems to door handles. Moreover, we will group objects in data libraries that contain Revit families to simplify stakeholders’ collaboration.',
-    },
-    {
-      subtitle: 'Our team identifies inconsistencies.',
-      description:
-        'Our team runs clash detection software to find and flag design conflicts.',
-    },
-    {
-      subtitle: 'We help resolve problems.',
-      description:
-        'We will send reports about the flaws in the design and propose solutions. Your team members will be able to work on a project simultaneously from a single file.',
-    },
-    {
-      subtitle: 'The client receives interactive 3D BIM models and shop drawings.',
-      description:
-        'You will get accurate fabrication drawings and a fully interactive 3D database that can tie construction and manufacturing sites for a significant productivity boost.',
-    },
-  ];
+  export const howModeling = {
+    title: 'BIM Coordination Process at Powerkh',
+    list: [
+      {
+        subtitle: 'The client sends the brief and technical documentation.',
+        description:
+          'We discuss minute details of the construction project and analyze drawings, 2D plans, blueprints, photos, and visuals references.',
+      },
+      {
+        subtitle:
+          'We import 2D drawings into the 3D model.',
+        description:
+          'Our team transforms flat drawings into a 3D model using Autodesk Revit software.',
+      },
+      {
+        subtitle: 'We build the framework and design elements.',
+        description:
+          'We digitalize all structural elements — from walls and MEP (mechanical, electrical, and plumbing) systems to door handles. Moreover, we will group objects in data libraries that contain Revit families to simplify stakeholders’ collaboration.',
+      },
+      {
+        subtitle: 'Our team identifies inconsistencies.',
+        description:
+          'Our team runs clash detection software to find and flag design conflicts.',
+      },
+      {
+        subtitle: 'We help resolve problems.',
+        description:
+          'We will send reports about the flaws in the design and propose solutions. Your team members will be able to work on a project simultaneously from a single file.',
+      }
+    ],
+  };
 
  
 
@@ -128,5 +127,30 @@
     description:
       'Do you want to enhance your construction project?Our experts can resolve your challenges and build a detailed 3D BIM model.Contact us to learn more about our services!',
   };
+
+  export const BIMmodelingFaq = [
+    {
+      question: 'How fast can I get a fully-realized BIM model?',
+      answer:
+        'Our delivery time depends on the project’s complexity (LOD), your standards, techniques, and the documentation we receive. Nonetheless, our team has extensive knowledge of BIM modeling software and can build an interactive model in the fastest possible time.',
+    },
+    {
+      question: 'Can you provide estimates for BIM modeling services?',
+      answer:
+        'We will provide a free quote with estimates based on our hourly fee. Furthermore, we will charge only after completing our work.',
+    },
+    {
+      question: 'Do you work in metric or imperial units?',
+     
+    },
+    {
+      question: 'How will we communicate during drafting?',
+      
+    },
+    {
+      question: 'What BIM modeling software do you use?',
+     
+    },
+  ];
   
   
--- src/js/bim-modeling/bim-modeling-section/bim-modeling-section-templates.js
+++ src/js/bim-modeling/bim-modeling-section/bim-modeling-section-templates.js
@@ -3,6 +3,7 @@ import servicesHBS from '../../../templates/architecAndStructBim.hbs';
 import heroHBS from '../../../templates/hero.hbs';
 import featuresForProjectsHBS from '../../../templates/outsourceProjects.hbs';
 import contactUsHBS from '../../../templates/contactUs.hbs';
+import faqTemplate from '../../../templates/faq.hbs';
 import herBIMAndVDC from '../../../templates/herBIMAndVDCServices.hbs';
 import {
   heroModeling,
@@ -11,6 +12,7 @@ import {
   servicesModeling,
   addValueModeling,
   contactUsModeling,
+  BIMmodelingFaq
 } from './bim-modeling-data';
 import { refs } from '../../common/refs-services';
 import { useTemplate } from '../../common/use-template';
@@ -20,4 +22,5 @@ useTemplate(refs.howWeWork, howWeWorkHBS, howModeling);
 useTemplate(refs.services, servicesHBS, servicesModeling);
 useTemplate(refs.herBIMAndVDC, herBIMAndVDC, BIMservices);
 useTemplate(refs.featuresForProjects, featuresForProjectsHBS, addValueModeling);
+useTemplate(refs.faq, faqTemplate, BIMmodelingFaq);
 useTemplate(refs.contactUs, contactUsHBS, contactUsModeling);
