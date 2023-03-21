f485f570f9c74863d84ad476221854397f9934c9
GrechkoseyNadezhda <Grechkosey.n@gmail.com>
2023-03-21 12:30:18 +0100

    scan to bim page

--- src/js/scan-to-bim/scan-to-bim-sections/scan-to-bim-create-templates.js
+++ src/js/scan-to-bim/scan-to-bim-sections/scan-to-bim-create-templates.js
@@ -2,10 +2,12 @@ import heroHBS from '../../../templates/hero.hbs';
 import herBIMAndVDCHBS from '../../../templates/herBIMAndVDCServices.hbs';
 import howWeWorkHBS from '../../../templates/howWeWork.hbs';
 import servicesHBS from '../../../templates/architecAndStructBim.hbs';
+import outsourceProjectsHBS from '../../../templates/outsourceProjects.hbs';
+import faqHBS from '../../../templates/faq.hbs';
 import contactUsHBS from '../../../templates/contactUs.hbs';
 
 import { fetchDatabaseAuthor } from '../../common/fetchDatabaseAuthor';
-
+import { fetchDatabaseFAQ } from '../../common/fetchDatabaseAuthor';
 import {
   heroScanToBIM,
   herBIMAndVDCScanToBIM,
@@ -22,6 +24,13 @@ useTemplate(refs.hero, heroHBS, heroScanToBIM);
 useTemplate(refs.herBIMAndVDC, herBIMAndVDCHBS, herBIMAndVDCScanToBIM);
 useTemplate(refs.howWeWork, howWeWorkHBS, howWeWorkScanToBIM);
 useTemplate(refs.services, servicesHBS, servicesScanToBIM);
+useTemplate(
+  refs.featuresForProjects,
+  outsourceProjectsHBS,
+  advantagesOfScanYoBIM
+);
+
 useTemplate(refs.contactUs, contactUsHBS, contactUsScanToBIM);
 
 // fetchDatabaseAuthor('/authors/KostiaRapina.json');
+// fetchDatabaseFAQ('/faq/scanToBim.json');
