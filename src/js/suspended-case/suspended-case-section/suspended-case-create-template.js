79a0bca7672120f892762a3feb72020931b37073
Tetiana-V-Kovalenko <113469589+Tetiana-V-Kovalenko@users.noreply.github.com>
2023-03-21 23:06:40 +0200

    for example slider

--- src/js/suspended-case/suspended-case-section/suspended-case-create-template.js
+++ src/js/suspended-case/suspended-case-section/suspended-case-create-template.js
@@ -1,4 +1,4 @@
-import Swiper, { Navigation, Pagination } from 'swiper';
+// import Swiper, { Navigation, Pagination } from 'swiper';
 import impactHBS from '../../../templates/impactOnBusiness.hbs';
 import herotextHBS from '../../../templates/heroDesignText.hbs';
 import sliderHBS from '../../../templates/heroDesignSlider.hbs';
@@ -9,15 +9,16 @@ import {
   impactSuspended,
 } from '../../suspended-case/suspended-case-section/suspended-case-data';
 import { refsCase } from '../../common/refs-services';
-const swiper = new Swiper('.swiper', {
-  modules: [Navigation, Pagination],
+// import { swiper } from '../../components/CaseHeroSlider';
+// const swiper = new Swiper('.swiper', {
+//   modules: [Navigation, Pagination],
 
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-    dynamicBullets: true,
-  },
-});
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//     dynamicBullets: true,
+//   },
+// });
 
 useTemplate(refsCase.heroTextBlock, herotextHBS, textHeroSuspended);
 useTemplate(refsCase.heroSlider, sliderHBS, sliderPictures);
