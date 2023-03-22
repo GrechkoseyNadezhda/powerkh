f2777776b02fddff8ad0e9bafd769dc94f23b6a0
Evgeniy Pavlichenko <yevheniipavl@gmail.com>
2023-03-22 12:41:48 +0200

    add FAQ

--- src/js/bim-content/bim-content-sections/bim-content-creat-template.js
+++ src/js/bim-content/bim-content-sections/bim-content-creat-template.js
@@ -12,14 +12,18 @@ import objLibraryCreationHBS from '../../../templates/howWeWork.hbs';
 import servicesHBS from '../../../templates/architecAndStructBim.hbs';
 import advantagesHBS from '../../../templates/outsourceProjects.hbs';
 import contactUsHBS from '../../../templates/contactUs.hbs';
+import faqHBS from '../../../templates/faq.hbs';
 
 import { refs } from '../../common/refs-services';
 import { useTemplate } from '../../common/use-template';
+import { createFAQsection } from '../../common/createFaqSection';
+
+const ENDPOINT_CONTENT = '/faq/bimContent.json';
 
 useTemplate(refs.hero, heroHBS, heroContentObj);
 useTemplate(refs.herBIMAndVDC, contentCreationHBS, contentCreationObj);
 useTemplate(refs.howWeWork, objLibraryCreationHBS, objLibraryCreationObj);
 useTemplate(refs.services, servicesHBS, servicesCreationObj);
 useTemplate(refs.featuresForProjects, advantagesHBS, advantagesObj);
-
 useTemplate(refs.contactUs, contactUsHBS, contactUsContentObj);
+createFAQsection(ENDPOINT_CONTENT, refs.faq, faqHBS);
