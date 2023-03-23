37ed4deef364b0f8e9d3d1152d05aebfb726e995
Olha Prymak <113449585+peckeby@users.noreply.github.com>
2023-03-23 20:23:14 +0200

    Formwork design page
    +main page styling

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import './js/main/outsourceProjects';
+
+// header
 import './js/components/header/header';
 import './js/main/heroMain.js';
 import './js/components/contactUsModal/contactUsModal';
 import './js/components/footer';
 import './js/main/herBIMandVDC_main.js';
 import './js/main/howWeWorkMain.js';
+
+import './js/components/footer';
---
+++ src/js/formwork-design-automation/formwork-design-template.js
@@ -0,0 +1,33 @@
+import Swiper, { Navigation, Pagination } from 'swiper';
+import processTemplateHBS from '../../templates/processTemplate.hbs';
+import solutionTemplateHBS from '../../templates/solution.hbs';
+import impactProcessTemplateHBS from '../../templates/impactOnBusiness.hbs';
+import herotextHBS from '../../templates/heroDesignText.hbs';
+import sliderHBS from '../../templates/heroDesignSlider.hbs';
+
+import {
+  processTemplate,
+  processSolution,
+  processImpact,
+  textHeroFormworkDesign,
+  sliderPicturesFormwork,
+} from './formwork-design-automation-data';
+import { refs } from '../common/refs-services';
+import { refsCase } from '../common/refs-services';
+import { useTemplate } from '../common/use-template';
+useTemplate(refs.process, processTemplateHBS, processTemplate);
+useTemplate(refs.solution, solutionTemplateHBS, processSolution);
+useTemplate(refsCase.impactOnBusiness, impactProcessTemplateHBS, processImpact);
+
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation, Pagination],
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+});
+
+useTemplate(refsCase.heroTextBlock, herotextHBS, textHeroFormworkDesign);
+useTemplate(refsCase.heroSlider, sliderHBS, sliderPicturesFormwork);
