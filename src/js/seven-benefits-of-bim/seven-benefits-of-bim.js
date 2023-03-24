ebc6c78d8f306d71edbc9b8d6128436f56f9e7d3
Yurii Oborskiy <yuriioborskiy@gmail.com>
2023-03-24 17:20:27 +0200

    fix: add scripts to blog-pages

--- src/js/seven-benefits-of-bim/seven-benefits-of-bim.js
+++ src/js/seven-benefits-of-bim/seven-benefits-of-bim.js
@@ -2,4 +2,4 @@ import './seven-benefits-of-bim-sections/seven-benefits-of-bim-create-templates'
 import '../components/header/header';
 import '../components/footer';
 // import '../components/modal-question/modal';
-// import '../components/contactUsModal/contactUsModal';
+import '../components/contactUsModal/contactUsModal';
