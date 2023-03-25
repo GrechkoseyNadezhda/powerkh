a48cc392c6780f3e66d2ea7d6d24b35cf858a78c
Evhenii Babii <babiyka09@gmail.com>
2023-03-25 14:34:20 +0200

    fixed arrow

--- src/js/bim-modeling/bim-modeling-section/bim-modeling-section-templates.js
+++ src/js/bim-modeling/bim-modeling-section/bim-modeling-section-templates.js
@@ -8,7 +8,6 @@ import featuresForProjectsHBS from '../../../templates/outsourceProjects.hbs';
 import contactUsHBS from '../../../templates/contactUs.hbs';
 import faqTemplate from '../../../templates/faq.hbs';
 import herBIMAndVDC from '../../../templates/herBIMAndVDCServices.hbs';
-import ourProjectsSliderHBS from '../../../templates/ourProjectsSlider.hbs';
 import sliderHBS from '../../../templates/ourProjectsSlider.hbs';
 
 import {
@@ -19,7 +18,7 @@ import {
   addValueModeling,
   contactUsModeling,
   BIMmodelingFaq,
-  // projectsSliderImg,
+  projectsSliderImg,
   projectCoordination,
 } from './bim-modeling-data';
 import { refs, refsCase } from '../../common/refs-services';
@@ -50,10 +49,7 @@ useTemplate(refs.howWeWork, howWeWorkHBS, howModeling);
 useTemplate(refs.services, servicesHBS, servicesModeling);
 useTemplate(refs.herBIMAndVDC, herBIMAndVDC, BIMservices);
 useTemplate(refs.featuresForProjects, featuresForProjectsHBS, addValueModeling);
-// useTemplate(refs.faq, faqTemplate, BIMmodelingFaq);
 useTemplate(refs.contactUs, contactUsHBS, contactUsModeling);
-useTemplate(refsCase.bmpSlider, ourProjectsSliderHBS, projectsSliderImg);
 useTemplate(refs.slider, sliderHBS, projectCoordination);
-
 createFAQsection(ENDPOINT_COORDINATION, refs.faq, faqTemplate);
 toggleFaq();
