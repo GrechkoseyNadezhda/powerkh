0f8670a5dceeb1d3595dacd922d99bf32af8ed93
Alex Koniushenko <alex.koniushenko@gmail.com>
2023-03-28 17:26:29 +0300

    Update case-studies-data.js

--- src/js/case-studies/case-studies-sections/case-studies-data.js
+++ src/js/case-studies/case-studies-sections/case-studies-data.js
@@ -14,7 +14,7 @@ export const caseStudiesData = [
     title: 'Formwork design automation',
     author: 'California, USA',
     description:
-      'The client struggled with a time-consuming workflow of floor assembly design. Our team implemented Dynamo scripts to speed up every subprocess and do quality control. In the end, the client got a smooth workflow and an efficient, error-free design process.',
+      'Our client had to deal with an inefficient formwork design process. They utilized CAD technology as the main design tool.',
     link: './formwork-design-automation.html',
     img1x: img11x,
     img2x: img12x,
@@ -25,7 +25,7 @@ export const caseStudiesData = [
     title: 'Suspended ceiling design automation',
     author: 'New York, USA',
     description:
-      'The client struggled with a time-consuming workflow of floor assembly design. Our team implemented Dynamo scripts to speed up every subprocess and do quality control. In the end, the client got a smooth workflow and an efficient, error-free design process.',
+      'The design process had a series of challenges. One of them was to design a drop ceiling with deck-suspended hangers. It takes quite a lot of time and effort to design it according to structural requirements, model it correctly, and coordinate it with other building elements. A drop ceiling is a system that includes many elements dependent on each other. Even the smallest changes require updating the whole system. As a result, the delivery time of the ready-to-use design goes up. On top of that, it took another 5-6 days for the framing hangers manufacturer to come up with design drawings needed for the Revit model. In the end, every update caused at least a one-week delay.',
     link: './suspended-case.html',
     img1x: img21x,
     img2x: img22x,
