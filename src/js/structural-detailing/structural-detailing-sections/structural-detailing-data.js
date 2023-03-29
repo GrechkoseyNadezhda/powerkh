7757c2ac6a0ddc07de91155c475737c74d892d7b
Yurii Oborskiy <yuriioborskiy@gmail.com>
2023-03-29 13:29:16 +0300

    fix: data-js

--- src/js/structural-detailing/structural-detailing-sections/structural-detailing-data.js
+++ src/js/structural-detailing/structural-detailing-sections/structural-detailing-data.js
@@ -10,39 +10,39 @@ export const heroStructuralDetailing = {
   text: 'Are you looking for a reliable partner? Powerkh delivers accurate 3D models, detailed shop drawings, and affordable structural detailing services. Save costs and eliminate design clashes by entrusting your BIM project to our capable hands!',
 };
 export const briefDescriptStructuralDetailing = {
-title: 'Structural Detailing Services',
-listText: [
-  'Construction projects must rely on accurate documentation to avoid costly reworks and missed deadlines. How can you make sure your shop drawings are error-free?',
-  'You will get a precise library of objects, detailed specifications for every element and system, and statements of production — a full package of documents to begin the manufacturing and construction!',
-  'Powerkh provides structural steel detailing services to help you find and resolve design errors and clashes before starting on-site work. Our teams of engineers and developers can refine your 3D models, shop drawings, and other construction documentation. We follow local construction codes and proven design principles to deliver quality results at a lower cost.',
-],
+  title: 'Structural Detailing Services',
+  listText: [
+    'Construction projects must rely on accurate documentation to avoid costly reworks and missed deadlines. How can you make sure your shop drawings are error-free?',
+    'You will get a precise library of objects, detailed specifications for every element and system, and statements of production — a full package of documents to begin the manufacturing and construction!',
+    'Powerkh provides structural steel detailing services to help you find and resolve design errors and clashes before starting on-site work. Our teams of engineers and developers can refine your 3D models, shop drawings, and other construction documentation. We follow local construction codes and proven design principles to deliver quality results at a lower cost.',
+  ],
 };
 export const howStructuralDetailing = {
   title: 'How We Work',
-list: [
-  {
-    subtitle: 'The client sends the documentation.',
-    description:
-      'Our professionals will run through your shop drawings and 3D models to find any conflicting elements and missing information.',
-  },
-  {
-    subtitle: 'We process your documents.',
-    description:
-      'We use advanced BIM modeling and clash detection software to resolve design conflicts and optimize construction logic.',
-  },
+  list: [
+    {
+      subtitle: 'The client sends the documentation.',
+      description:
+        'Our professionals will run through your shop drawings and 3D models to find any conflicting elements and missing information.',
+    },
+    {
+      subtitle: 'We process your documents.',
+      description:
+        'We use advanced BIM modeling and clash detection software to resolve design conflicts and optimize construction logic.',
+    },
 
-  {
-    subtitle: 'We create a 3D model and drawings.',
-    description:
-      'Our team can build a parametric visual database and 2D drawings with data about every object — from walls down to the tiniest screw.',
-  },
-  {
-    subtitle: 'The client receives a detailed BIM model.',
-    description:
-      'We will deliver an interactive 3D BIM model, design plans, fabrication drawings, BOMs (bills of materials), material takeoff, and other documents.',
-  },
-]
-}
+    {
+      subtitle: 'We create a 3D model and drawings.',
+      description:
+        'Our team can build a parametric visual database and 2D drawings with data about every object — from walls down to the tiniest screw.',
+    },
+    {
+      subtitle: 'The client receives a detailed BIM model.',
+      description:
+        'We will deliver an interactive 3D BIM model, design plans, fabrication drawings, BOMs (bills of materials), material takeoff, and other documents.',
+    },
+  ],
+};
 
 export const servicesStructuralDetailing = {
   title: 'Our Structural Detailing Services',
@@ -50,14 +50,14 @@ export const servicesStructuralDetailing = {
   topic: 'Our services include:',
   list: [
     'Steel detailing for architectural and structural systems',
-    'Detailing for precast and prefabrication elements (hybrid joints, beams, bar joists)',
-    'Curtain wall detailing',
-    'Concrete outline drawings',
-    'Statements of production (bills of materials, material takeoff, and other documents)',
     'Rebar detailing',
+    'Detailing for precast and prefabrication elements (hybrid joints, beams, bar joists)',
     'Structural 3D BIM detailing',
+    'Curtain wall detailing',
     'Shop (fabrication) drawings',
+    'Concrete outline drawings',
     'BIM coordination services',
+    'Statements of production (bills of materials, material takeoff, and other documents)',
     'Construction strategy development',
   ],
 };
@@ -65,7 +65,8 @@ export const addValueStructuralDetailing = {
   title: 'Why Choose Powerkh as Structural Detailers',
   description:
     'Steel detailing is a complex service that involves exact dimensional accuracy for every little detail. However, our refined approach helps us automate recurring tasks to deliver fast and error-free results. Powerkh delivers affordable steel detailing solutions for commercial and industrial clients in the US, UK, and Europe.',
-  listDescription: 'Here’s what you can expect if you outsource structural detailing services to us:',
+  listDescription:
+    'Here’s what you can expect if you outsource structural detailing services to us:',
   items: [
     {
       subtitle: 'Engineering expertise',
@@ -75,12 +76,12 @@ export const addValueStructuralDetailing = {
     {
       subtitle: 'Software mastery',
       subtitleDescription:
-      'We use advanced software to build accurate BIM models and translate them into detailed 2D drawings.',
+        'We use advanced software to build accurate BIM models and translate them into detailed 2D drawings.',
     },
     {
       subtitle: 'Fast turnaround time',
       subtitleDescription:
-      'Extensive knowledge and BIM automation tools allow us to deliver results faster than the competition.',
+        'Extensive knowledge and BIM automation tools allow us to deliver results faster than the competition.',
     },
     {
       subtitle: 'Project coordination',
